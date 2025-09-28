2eec29820a8f762ce377fe73ecb0248aceeb4c12
ayowayne123 <ayowayne123@gmail.com>
2025-09-28 01:40:19 +0100

    fix: context provider

--- apps/homes/app/layout.js
+++ apps/homes/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
 import PluggedIn from "@/components/ui/pluggedIn";
+import { ListingsProvider } from "@/components/context/listingsContext";
 
 const wixFont = Wix_Madefor_Display({
   variable: "--font-wix-madefor-display",
@@ -17,10 +18,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${wixFont.className} antialiased`}>
-        <Header />
-        {children}
-        <PluggedIn />
-        <Footer />
+        <ListingsProvider>
+          <Header />
+          {children}
+          <PluggedIn />
+          <Footer />
+        </ListingsProvider>
       </body>
     </html>
   );
