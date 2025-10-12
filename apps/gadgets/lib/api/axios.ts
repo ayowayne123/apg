63e083bcd34dbccdb29452695631290aa222bd2b
ayowayne123 <ayowayne123@gmail.com>
2025-10-12 19:10:12 +0100

    Feat: Adding API Base Url

--- apps/gadgets/lib/api/axios.ts
+++ apps/gadgets/lib/api/axios.ts
@@ -1,7 +1,8 @@
 // axios.ts
 import axios from "axios";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+const API_BASE =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.apgbusinesshub.com";
 
 const api = axios.create({
   baseURL: API_BASE,
