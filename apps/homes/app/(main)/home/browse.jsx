34720f4c037819223e7e7114d539b8fa7e33e5a1
ayowayne123 <ayowayne123@gmail.com>
2025-11-28 20:01:04 +0100

    Fix: Coverphoto Url issue fixed

--- apps/homes/app/(main)/home/browse.jsx
+++ apps/homes/app/(main)/home/browse.jsx
@@ -49,23 +49,23 @@ function Browse() {
       <div className="cardWidth sm:overflow-x-auto no-scrollbar py-4">
         <div className="flex sm:flex-row flex-col gap-6 w-full md:mr-20 mr-10 lg:mr-24">
           {loading ? (
-            [...Array(4)].map((_, i) => (
-             <HomeCardSkeleton/>
-            ))
+            [...Array(4)].map((_, i) => <HomeCardSkeleton />)
           ) : error ? (
             <p className="text-center text-red-500 w-full">{error}</p>
           ) : homes.length > 0 ? (
             homes.map((property, index) => (
               <Link
                 key={index}
-                href={`/listing/${property.slug}`}
-                className="bg-[#F5F5F5] rounded-[20px] hover:shadow-lg transition shrink-0 sm:w-[300px] sm:h-[450px] flex flex-col overflow-hidden p-4"
+                href={`/listing/${property?.slug}`}
+                className="bg-[#F5F5F5] rounded-[20px] hover:shadow-lg transition shrink-0 sm:w-[300px] sm:h-[480px] flex flex-col overflow-hidden p-4"
               >
                 {/* Image */}
-                <div className="relative w-full h-[250px] rounded-2xl overflow-hidden">
+                <div className="relative w-full h-[250px] shrink-0 rounded-2xl overflow-hidden">
                   <Image
-                    src={property.cover_photo.url || "/images/house.jpg"}
-                    alt={property.cover_photo.url || property.title}
+                    src={property?.cover_photo?.url || "/images/house.jpg"}
+                    alt={
+                      property?.cover_photo?.url || property?.title || "A House"
+                    }
                     fill
                     className="object-cover"
                   />
@@ -76,26 +76,31 @@ function Browse() {
 
                 {/* Card Content */}
                 <div className="mt-5 flex flex-col gap-2">
-                  <h3 className="text-2xl font-bold text-primary">
-                    {property.title}
+                  <h3 className="text-2xl line-clamp-2  font-bold text-primary ">
+                    {property?.title}
                   </h3>
-                  <p className="text-xl font-bold">{property.price}</p>
+                  <p className="text-xl font-bold capitalize">
+                    {property.currency === "USD" ? "$" : "₦"}
+                    {Number(property.price).toLocaleString()}
+                    {property?.listing_type !== "sale" &&
+                      `/${property?.payment_frequency}`}
+                  </p>
                   <p className="text-sm text-greyText flex items-center gap-2">
-                    {property.location}
+                    {property.city}
                   </p>
 
                   {/* Meta Info */}
                   <div className="flex items-center justify-between text-sm text-greyText mt-3">
                     <span className="flex items-center gap-1">
-                      <LuBedDouble size={14} /> {property.beds}bd
+                      <LuBedDouble size={14} /> {property?.bedrooms}bed
                     </span>
                     <div className="h-4 w-px bg-gray-400" />
                     <span className="flex items-center gap-1">
-                      <Bath size={14} /> {property.baths}ba
+                      <Bath size={14} /> {property?.bathrooms}bath
                     </span>
                     <div className="h-4 w-px bg-gray-400" />
                     <span className="flex items-center gap-1">
-                      <RiRuler2Line size={14} /> {property.size}
+                      <RiRuler2Line size={14} /> {property?.size}m<sup>2</sup>
                     </span>
                   </div>
                 </div>
